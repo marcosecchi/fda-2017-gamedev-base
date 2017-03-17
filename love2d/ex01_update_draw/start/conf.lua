425e1fea8fd059d6cd87b9cc7fc3a35beb5d23ce
Marco Secchi <loziosecchi@gmail.com>
2017-03-17 19:11:26 +0100

    Add update draw lifecycle example.

---
+++ love2d/ex01_update_draw/start/conf.lua
@@ -0,0 +1,4 @@
+function love.conf(t)
+  -- Qui vengono inseriti i valori di configurazione generale del gioco,
+  -- ad esempio dimensioni della finestra, titolo, etc.
+end
---
+++ love2d/ex01_update_draw/start/main.lua
@@ -0,0 +1,14 @@
+function love.load(arg)
+  -- Questa funzione di 'callback' viene chiamata una sola volta all'inizio del gioco
+  -- e serve per inizializzare i valori che verranno utilizzati durante il gioco
+end
+
+function love.update(dt)
+  -- Questa funzione di 'callback' viene chiamata ad ogni ciclo e viene utilizzata
+  -- per calcolare la logica del gioco
+end
+
+function love.draw()
+  -- Questa funzione di 'callback' viene chiamata ad ogni ciclo e viene utilizzata
+  -- per disegnare la grafica sullo schermo
+end
