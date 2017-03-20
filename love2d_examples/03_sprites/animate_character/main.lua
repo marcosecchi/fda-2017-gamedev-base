a7dacba4881082f5f328fc25d756f24196b33ed3
Marco Secchi <loziosecchi@gmail.com>
2017-03-20 20:22:22 +0100

    Update comments.

--- love2d_examples/03_sprites/animate_character/main.lua
+++ love2d_examples/03_sprites/animate_character/main.lua
@@ -2,25 +2,25 @@ function love.load(arg)
   cowboy = dofile("cowboy.lua")
   cowgirl = dofile("cowgirl.lua")
 
-  frame = 1;
-  selectedCharacter = cowboy
-  frameDuration = 0.10
-  timeElapsed = 0
+  frame = 1;  -- definisci il frame selezionato
+  selectedCharacter = cowboy  -- definisci il personaggio selezionato
+  frameDuration = 0.10  -- definisci la durata di un frame
+  timeElapsed = 0 -- calcola il tempo per il cambiamento dei frame
 end
 
 function love.update(dt)
-  timeElapsed = timeElapsed + dt
+  timeElapsed = timeElapsed + dt -- aggiorna il tempo passato dall'ultimo cambio di frame
 
-  if(timeElapsed >= frameDuration) then
-    frame = frame + 1
-    if frame > table.getn(selectedCharacter.animations) then
-      frame = 1
+  if(timeElapsed >= frameDuration) then -- se è ora di cambiare frame...
+    frame = frame + 1 -- ... aggiorna l'indice
+    if frame > table.getn(selectedCharacter.animations) then -- se l'indice va oltre il numero di element dell'animazione...
+      frame = 1 -- ... ritorna al primo frame
     end
-    timeElapsed = 0
+    timeElapsed = 0 -- azzera il contatore del tempo
   end
 end
 
 function love.draw()
-  image = love.graphics.newImage(selectedCharacter.animations[frame])
-  love.graphics.draw(image, 100, 100)
+  image = love.graphics.newImage(selectedCharacter.animations[frame]) -- carica l'immagine corretta dalla mappa
+  love.graphics.draw(image, 100, 100) -- disegna l'immagine a schermo
 end
